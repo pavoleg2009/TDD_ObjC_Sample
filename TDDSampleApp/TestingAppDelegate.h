464cf44d8e968ad11fa434ea92a0adf060b60043
Oleg Pavlichenkov <pavoleg@mail.ru>
2017-04-18 21:25:18 +0300

    TestingAppDelegate added

---
+++ TDDSampleApp/TestingAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TestingAppDelegate.h
+//  TDDSampleApp
+//
+//  Created by Oleg Pavlichenkov on 18/04/2017.
+//  Copyright © 2017 Oleg Pavlichenkov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface TestingAppDelegate : NSObject
+@property (nonatomic, strong) UIWindow *window;
+@end
---
+++ TDDSampleApp/TestingAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  TestingAppDelegate.m
+//  TDDSampleApp
+//
+//  Created by Oleg Pavlichenkov on 18/04/2017.
+//  Copyright © 2017 Oleg Pavlichenkov. All rights reserved.
+//
+
+#import "TestingAppDelegate.h"
+
+@implementation TestingAppDelegate
+
+@end
